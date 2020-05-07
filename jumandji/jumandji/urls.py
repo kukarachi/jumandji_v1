c628c79beece436164319d27759ddd3b4c832197
KUKARACHI <rahim.balabayov@gmail.com>
2020-05-08 01:13:20 +0400

    initial commit

--- jumandji/jumandji/urls.py
+++ jumandji/jumandji/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from vacancies.views import CompanyView, MainView, VacancyView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', MainView.as_view()),
+    path('companies/<int:id>/', CompanyView.as_view()),
+    re_path(r'^vacancies/?(?P<id>\d+)?/?(cat)?/?(?P<code>\w+)?/?', VacancyView.as_view())
 ]
--- jumandji/vacancies/models.py
+++ jumandji/vacancies/models.py
@@ -2,24 +2,26 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=64)
-    location = models.CharField(max_length=64)
-    logo = ""
-    description = models.CharField(max_length=128)
-    employee_count = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64, null=True)
+    location = models.CharField(max_length=64, null=True)
+    logo = models.CharField(max_length=128, default="https://place-hold.it/100x60")  # "https://place-hold.it/100x60"
+    description = models.CharField(max_length=128, null=True)
+    employee_count = models.IntegerField(null=True)
 
 
 class Specialty(models.Model):
     code = models.CharField(max_length=32)
     title = models.CharField(max_length=64)
-    picture = ""
+    picture = models.CharField(max_length=128, default="https://place-hold.it/100x60")  # "https://place-hold.it/100x60"
 
 
 class Vacancy(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name='vacancies')
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
-    skills = models.CharField(max_length=128)
+    skills = models.CharField(max_length=128, null=True)
     description = models.CharField(max_length=128)
     salary_min = models.IntegerField()
     salary_max = models.IntegerField()
